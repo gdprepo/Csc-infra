83d2bfab769704797b10274aca87ef2a09a3e01f
dgabin <depairegabin@macbook-pro-de-macbook-1.home>
2020-06-11 17:15:36 +0200

    upd link

--- csc/public/js/selectText.js
+++
@@ -1,8 +0,0 @@
-// function styleselect() {
-//     var value = $('#globalstyleselect').val();
-//     let element = document.getElementById(value);
-//     element.style.display = "block";
-
-// }
-
-
